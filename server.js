0ec8cbf5b4239dd491d872465601955d55608cdb
JoeVincentt <eugene.bb@hotmail.com>
2018-12-27 19:52:39 -0500

    conver id to string

--- server.js
+++ server.js
@@ -45,7 +45,16 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return events;
+        return Event.find()
+          .then(events => {
+            return events.map(event => {
+              //convert _id from Mongo to string
+              return { ...event._doc, _id: event._doc._id.toString() };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
       },
       createEvent: args => {
         const event = new Event({
@@ -58,7 +67,7 @@ app.use(
           .save()
           .then(result => {
             console.log(result);
-            return { ...result._doc };
+            return { ...result._doc, _id: result._doc._id.toString() };
           })
           .catch(err => {
             console.log(err);
