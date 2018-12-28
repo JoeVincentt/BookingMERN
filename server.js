949ca8849a332d37c84cc8942f2e3072af6779f4
JoeVincentt <eugene.bb@hotmail.com>
2018-12-27 20:49:10 -0500

    check if email exists and throw an error

--- server.js
+++ server.js
@@ -89,8 +89,13 @@ app.use(
           });
       },
       createUser: args => {
-        return bcrypt
-          .hash(args.userInput.password, 12)
+        return User.findOne({ email: args.userInput.email })
+          .then(user => {
+            if (user) {
+              throw new Error("User exists already.");
+            }
+            return bcrypt.hash(args.userInput.password, 12);
+          })
           .then(hashedPassword => {
             const user = new User({
               email: args.userInput.email,
@@ -99,7 +104,6 @@ app.use(
             return user.save();
           })
           .then(result => {
-            console.log(result);
             return { ...result._doc, password: null, _id: result.id };
           })
           .catch(err => {
