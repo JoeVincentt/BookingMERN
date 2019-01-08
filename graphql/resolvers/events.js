d7bfb61cb744652d498f32fa5b8eb3b7c1720997
JoeVincentt <eugene.bb@hotmail.com>
2019-01-07 22:17:36 -0500

    refectored code found bugs trying to fix

---
+++ graphql/resolvers/events.js
@@ -0,0 +1,40 @@
+const { transformEvent } = require("./merge");
+const Event = require("../../models/events");
+
+module.exports = {
+  events: async () => {
+    try {
+      const events = await Event.find();
+      return events.map(event => {
+        //convert _id from Mongo to string
+        return transformEvent(event);
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+  createEvent: async args => {
+    const event = new Event({
+      title: args.eventInput.title,
+      description: args.eventInput.description,
+      price: +args.eventInput.price,
+      date: new Date(args.eventInput.date),
+      creator: "5c2580efb1664e1543fdf018"
+    });
+    let createdEvent;
+    try {
+      const result = await event.save();
+      createdEvent = transformEvent(result);
+      const creatorUser = await User.findById("5c2580efb1664e1543fdf018");
+
+      if (!creatorUser) {
+        throw new Error("No Users Found.");
+      }
+      creatorUser.createdEvents.push(event);
+      await creatorUser.save();
+      return createdEvent;
+    } catch (err) {
+      throw err;
+    }
+  }
+};
